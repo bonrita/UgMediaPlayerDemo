ee464e67b6c022b57172496e9b321bf1116a714e
Bona <bona.wani@mediamonks.com>
2017-02-12 17:28:56 +0100

    Add volley package.

--- app/src/main/java/com/bonrita/mediaplayerdemo/MainActivity.java
+++ app/src/main/java/com/bonrita/mediaplayerdemo/MainActivity.java
@@ -1,7 +1,11 @@
 package com.bonrita.mediaplayerdemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,5 +17,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Override the default actionbar so as i use the toolbar.
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(fabOnclickListener);
+
     }
+
+    View.OnClickListener fabOnclickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Toast.makeText(getApplicationContext(), "Fab btn clicked", Toast.LENGTH_LONG).show();
+        }
+    };
+
 }
