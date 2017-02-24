b34efe05f3d2e3965add847e94689cb4fd69cf14
Bona <bona.wani@mediamonks.com>
2017-02-24 20:36:41 +0100

    Add current audio from the audio list.;

--- app/src/main/java/com/bonrita/mediaplayerdemo/MediaPlayerService.java
+++ app/src/main/java/com/bonrita/mediaplayerdemo/MediaPlayerService.java
@@ -6,14 +6,16 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.media.session.MediaSessionManager;
 import android.os.Binder;
 import android.os.IBinder;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
-public class MediaPlayerService extends Service implements MediaPlayer.OnCompletionListener {
+public class MediaPlayerService extends Service implements MediaPlayer.OnCompletionListener, MediaPlayer.OnPreparedListener {
 
     // The binder to be given to clients.
     private final IBinder iBinder = new MediaPlayerBinder();
@@ -36,15 +38,60 @@ public class MediaPlayerService extends Service implements MediaPlayer.OnComplet
         //Get audio list data from shared preferences.
         MediaStorageUtility mediaStorage = new MediaStorageUtility(getApplicationContext());
         audioList = mediaStorage.loadAudioList();
+        currentAudioPosition = mediaStorage.getCurrentAudioPosition();
 
-        if(mediaSessionManager == null) {
+        if (currentAudioPosition != -1 && currentAudioPosition < audioList.size()) {
+            currentAudio = audioList.get(currentAudioPosition);
+        } else {
+            stopSelf();
+        }
+
+        if (mediaSessionManager == null) {
             initMediaPlayer();
         }
 
         return super.onStartCommand(intent, flags, startId);
     }
 
-    private BroadcastReceiver playNewAudio = new BroadcastReceiver() {
+    private void initMediaPlayer() {
+        if (mediaPlayer == null) {
+            mediaPlayer = new MediaPlayer();
+        }
+
+        // Set up the MediaPlayer event listeners.
+        mediaPlayer.setOnCompletionListener(this);
+
+        // Called to update status in buffering a media stream received through progressive HTTP download.
+        mediaPlayer.setOnPreparedListener(this);
+
+        // Reset so that the MediaPlayer is not pointing to another data source
+        mediaPlayer.reset();
+
+        mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+
+        try {
+            // Set the url of the audio.
+            mediaPlayer.setDataSource(currentAudio.getUrl());
+        } catch (IOException e) {
+            e.printStackTrace();
+            stopSelf();
+        }
+        mediaPlayer.prepareAsync();
+    }
+
+    /**
+     * Stop the current audio that is playing from playing.
+     */
+    private void stopCurrentAudioPlay() {
+        if (mediaPlayer == null) {
+            return;
+        } else if (mediaPlayer.isPlaying()) {
+            mediaPlayer.stop();
+        }
+    }
+
+
+    private BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
 
         @Override
         public void onReceive(Context context, Intent intent) {
@@ -62,34 +109,13 @@ public class MediaPlayerService extends Service implements MediaPlayer.OnComplet
         }
     };
 
-    private void initMediaPlayer() {
-        if (mediaPlayer == null) {
-            mediaPlayer = new MediaPlayer();
-        }
-
-        // Set up the MediaPlayer event listeners.
-        mediaPlayer.setOnCompletionListener(this);
-
-    }
-
-    /**
-     * Stop the current audio that is playing from playing.
-     */
-    private void stopCurrentAudioPlay() {
-        if (mediaPlayer == null) {
-            return;
-        } else if (mediaPlayer.isPlaying()) {
-            mediaPlayer.stop();
-        }
-    }
-
     /**
      * Grab the broadcast message that was broadcasted by the client (activity).
      * And then register the broadcast receiver that will act up on this broadcast.
      */
     private void registerActivityBroadcastPlayNewAudio() {
         IntentFilter filter = new IntentFilter(MainActivity.Broadcast_PLAY_NEW_AUDIO);
-        registerReceiver(playNewAudio, filter);
+        registerReceiver(broadcastReceiver, filter);
     }
 
     @Override
@@ -104,6 +130,18 @@ public class MediaPlayerService extends Service implements MediaPlayer.OnComplet
 
         // Stop this service.
         stopSelf();
+    }
+
+    @Override
+    public void onPrepared(MediaPlayer mp) {
+        // This is invoked when the media source is ready for playback
+        playAudio();
+    }
+
+    private void playAudio() {
+        if (!mediaPlayer.isPlaying()) {
+            mediaPlayer.start();
+        }
     }
 
     public class MediaPlayerBinder extends Binder {
